dd795aaf10e1f4a038667851aeae8f5b8da94cae
Paweł Dębski <pawel.debski.99@gmail.com>
2020-11-28 12:23:28 +0100

    Created a main view controller.

---
+++ Color_Clusterizer/Color_Clusterizer/Controllers/MainViewController.cs
@@ -0,0 +1,13 @@
+﻿using Color_Clusterizer.ExternalDependencies;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Color_Clusterizer.Controllers
+{
+    class MainViewController
+    {
+        public BitmapWrapper ClusteredImage { get; set; }
+        public MainViewController() { }
+    }
+}
