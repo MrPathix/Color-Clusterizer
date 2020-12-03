f8b0e412a838a5d7aa58093eb1ac31d6416dd0c8
Paweł Dębski <pawel.debski.99@gmail.com>
2020-12-03 21:00:47 +0100

    Added context menu for the main PictureBox.

--- Color_Clusterizer/Color_Clusterizer/ButtonHandlers/ClusterImageButtonHandler.cs
+++ Color_Clusterizer/Color_Clusterizer/ButtonHandlers/ClusterImageButtonHandler.cs
@@ -8,7 +8,7 @@ namespace Color_Clusterizer
 {
     public partial class ColorClusterizer
     {
-        private async void ClusterImageButtonHandler(object sender, EventArgs e)
+        private void ClusterImageButtonHandler(object sender, EventArgs e)
         {
             if (Controller.ClusteredImage is null)
             {
@@ -28,8 +28,6 @@ namespace Color_Clusterizer
             List<Task> tasks = new();
             tasks.Add(Task.Run(async () => kmeansPictureBox.Image = await kmeansTask));
             tasks.Add(Task.Run(async () => popularityPictureBox.Image = await popularityTask));
-
-            //await Task.WhenAll(tasks);
         }
     }
 }
